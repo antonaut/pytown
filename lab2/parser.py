92afdc4602367cec278c2e640b63260d74f7a1a1
Max Nordlund <max.nordlund@gmail.com>
2012-03-21 11:13:14 +0100

    Fixed parser bug

--- lab2/parser.py
+++ lab2/parser.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 from fd import Functional_Dependencies as FD
-from fdutils import *
+
 def parse(file):
     res = []
     with open(file) as f:
         for line in f:
             l, _, r = line.partition(" -> ")
             left  = set(l.split(", "))
-            right = set(r.split(", "))
+            right = set(r.strip().split(", "))
             res.append(FD(left, right))
     return res
-
-
-if __name__== "__main__":
-    fdz= parse("input/input1.dep")
-    print( closure({'A','C'} , fdz) )
