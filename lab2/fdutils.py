6e6e07d3746d7b6050d1569d96319611370a13f9
Anton Erholt <aerholt@kth.se>
2012-03-21 11:06:41 +0100

    Added faulty implementation of closure computation.

---
+++ lab2/fdutils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def closure(attrs, fds):
+    new_fds = set()
+    closure = attrs
+
+    for fd in fds:
+        new_fds = new_fds | fd.split()
+
+    done = False
+
+    while not done:
+        done = True # assume we are done
+        for fd in new_fds:
+            if fd.left() in closure and fd.right() not in closure:
+                closure = closure | fd.right()
+                done = False
+
+
+    return closure
+
+
+def projection():
+    pass
--- lab2/parser.py
+++ lab2/parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from fd import Functional_Dependencies as FD
-
+from fdutils import *
 def parse(file):
     res = []
     with open(file) as f:
@@ -14,4 +14,4 @@ def parse(file):
 
 if __name__== "__main__":
     fdz= parse("input/input1.dep")
-    print(fdz)
+    print( closure({'A','C'} , fdz) )
