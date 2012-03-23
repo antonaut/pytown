0078931d200eb533755965950691d97b325892f3
Max Nordlund <max.nordlund@gmail.com>
2012-03-23 10:59:26 +0100

    Fixed closure, projection and added diagrams!

--- lab2/fdutils.py
+++ lab2/fdutils.py
@@ -4,7 +4,7 @@ from fd import Functional_Dependencies as FD
 
 def closure(attrs, fds):
     new_fds = set()
-    closure = attrs
+    closure = attrs.copy()
     for fd in fds:
         new_fds |= fd.split()
 
@@ -19,22 +19,17 @@ def closure(attrs, fds):
 
 def projection(R, R1, S):
     T = set()
-    for a in combs(R1):
-        c = closure(a, S)
-        for fd in S:
-            if fd.right() < c and fd.right() < R1:
-                T.add(fd)
-    print(T)
-    #tmp = set()
-    #for fd in T:
-    #    tmp |= fd.split()
+    for X in combs(R1):
+        Xp = closure(X, S)
+        for A in R1:
+            if A in Xp and {A} != X:
+                T.add(FD(X, {A}))
     
     tmp = T.copy()
     for fd1 in tmp:
         for fd2 in tmp:
             if fd1.right() == fd2.left():
                 T.discard(FD(fd1.left(), fd2.right()))
-    print(T)
     
     tmp = T.copy()
     for fd in tmp:
@@ -42,12 +37,11 @@ def projection(R, R1, S):
         B = fd.right()
         l = len(Y)
         if l >= 2:
-            for Z in combinations(Y, l - 1):
-                fd1 = FD(Z, B)
+            for Z in combs2(Y, l):
                 for fd2 in tmp:
-                    if fd1.right() == fd2.left():
+                    if Z == fd2.right():
                         T.discard(fd)
-                        T.add(fd1)
+                        T.add(FD(Z, B))
     
     return T
 
@@ -55,3 +49,7 @@ def combs(rel):
     for i in range(1, len(rel)):
         for j in combinations(rel, i):
             yield set(j)
+
+def combs2(Y, l):
+    for x in combinations(Y, l - 1):
+        yield set(x)
--- lab2/test.py
+++ lab2/test.py
@@ -17,5 +17,5 @@ c1 = closure({'A','C'}, fd1)
 R  = {'A','B','C','D'}
 R1 = {'A','C','D'}
 S  = {FD({'A'},{'B'}),FD({'B'},{'C'}),FD({'C'},{'D'})}
-c = closure({'A'},S)
+c  = closure({'A'},S)
 p1 = projection(R, R1, S)
